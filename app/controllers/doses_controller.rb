1b48f662c2ba53197c83ae1e3b96bc6ae5ebcedf
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-15 17:33:58 +0100

    added delete

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,6 +16,12 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
   private
 
   def dose_params
